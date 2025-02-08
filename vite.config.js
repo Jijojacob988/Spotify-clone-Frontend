7bb6d1a8538a33f4eecb023a599937103c5a0099
Jijo Jacob <Jijojacob988@gmail.com>
2025-02-08 11:06:58 +0530

    Fix React Router 404 issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure build output is in 'dist'
-  }
-})
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Fixes 404 on refresh
+  },
+});
